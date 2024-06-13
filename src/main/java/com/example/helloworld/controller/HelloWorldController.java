955f9c2a577675b5931aef6445bfd314e31906f7
Vijayb8095 <103432007+Vijayb8095@users.noreply.github.com>
2024-06-13 11:48:03 +0530

    Update HelloWorldController.java
    Added welcome to the string

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "hi, Hello, World!";
+        return "Welocme, Hello, World!";
     }
 }
