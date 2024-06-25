0823d7ad66058a699f8d3cf279bd7477d86a7a23
Vijayb8095 <103432007+Vijayb8095@users.noreply.github.com>
2024-06-25 11:55:36 +0530

    Update HelloWorldApplication.java
    imported package

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,6 +2,8 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class HelloWorldApplication extends SpringBootServletInitializer{
